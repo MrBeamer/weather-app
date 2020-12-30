685e79c6e674e47e1e4d1ffbcecfacc4e9d1bef9
michael beamer <beamermichael@gmail.com>
2020-12-30 20:39:06 +0100

    added background, searchbar and basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+const api = {
+key: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
+base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+
+const dateMaker = (day) =. {
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+          type="text"
+          className="search-bar"
+          placeholder="Search"
+          />
+        </div>
+        <div className="location-box">
+          <div className="loaction">Berlin, Germany</div>
+          <div className="date">(dateMaker(new Date()))</div>
+        </div>
+      </main>
     </div>
   );
 }
