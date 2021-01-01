b35af460a6a975dd6e50b41f83af93b99df2b349
michael beamer <beamermichael@gmail.com>
2021-01-01 16:26:37 +0100

    added userState and event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import dateMaker from "./components/dateMaker.js"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const api = {
@@ -10,13 +10,18 @@ base: "https://api.openweathermap.org/data/2.5/"
 
 function App() {
 
-   const queryState = useState( '' );
-   const query = queryState[0]; // Contains ''
-   const setQuery = queryState[1]; // It’s a function
+   // const queryState = useState( "" );
+   // const query = queryState[0]; // Contains ''
+   // const setQuery = queryState[1]; // It’s a function
 
-   const weatherState = useState( {} );
-   const weather = weatherState[0]; // Contains ''
-   const setWeather = weatherState[1]; // It’s a function
+   // const weatherState = useState( {} );
+   // const weather = weatherState[0]; // Contains ''
+   // const setWeather = weatherState[1]; // It’s a function
+
+// array destructuring syntax
+   const [query, setQuery] = useState( "" );
+   const [weather, setWeather] = useState( {} );
+
 
 
      const search = event => {
@@ -27,32 +32,9 @@ function App() {
           setWeather(data);
           setQuery('');
           console.log(data);
-          console.log(data.name);
-          console.log(data.main.temp);
-          console.log(data.sys.country);
         });
     }
   }
-
-  // getting weather by coordinates
-const fetchWeatherByCoordinates = (lat, lon) => {
-  fetch(`${api.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${api.key}`)
-    .then(response => response.json())
-    .then((data) => {
-      setWeather(data);
-      console.log(data);
-      });
-    }
-
-
-// nav and coordinates
-navigator.geolocation.getCurrentPosition((data) => {
-  const latitude = data.coords.latitude;
-  const longitude = data.coords.longitude;
-  fetchWeatherByCoordinates(latitude, longitude);
-});
-
-
 
   return (
     <div className="app">
@@ -67,22 +49,26 @@ navigator.geolocation.getCurrentPosition((data) => {
             onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">
-            Berlin, Germany
+         {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">
+                {dateMaker()}
+              </div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">
+                sunny
+              </div>
+            </div>
           </div>
-          <div className="date">
-            {dateMaker()}
-          </div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            4°C
-          </div>
-          <div className="weather">
-            sunny
-          </div>
-        </div>
+          ) : ('')}
       </main>
     </div>
   );
